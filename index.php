b4c78a34c288fe060cd5313d2f5a0b3d321d6754
Celine <celine.pro.morel@gmail.com>
2024-04-30 15:52:16 +0200

    ajout formulaire

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <fieldset disabled>
+            <legend>Clone-Wetransfer</legend>
+            <div class="mb-3">
+                <label for="disabledTextInput" class="form-label">Fichier</label>
+
+                <div class="mb-3">
+                    <label for="disabledSelect" class="form-label">Email destinataire</label>
+
+
+                    <div class="mb-3">
+                        <label for="disabledSelect" class="form-label">Email</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Send</button>
+        </fieldset>
+    </form>
+</body>
+
+</html>
