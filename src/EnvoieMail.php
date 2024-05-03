360537cb6e6a87c606bb5a34b57e8688022841d1
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-05-03 10:10:08 +0200

    Test Email

--- src/EnvoieMail.php
+++ src/EnvoieMail.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -24,20 +24,20 @@ try {
 
     //Recipients
     $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    // $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addAddress('sylvainlacroix@protonmail.com', 'Sylvain LACROIX');     //Add a recipient
+    $mail->addAddress('celine.pro.morel@gmail.com');               //Name is optional
     // $mail->addReplyTo('info@example.com', 'Information');
     // $mail->addCC('cc@example.com');
     // $mail->addBCC('bcc@example.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('../uploads/closetotheedge-yes.jpg');         //Add attachments
     // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'Test Weetransfert Mailer';
+    $mail->Body    = 'TEST!!';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     //To load the French version
