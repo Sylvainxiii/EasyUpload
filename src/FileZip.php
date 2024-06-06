bc9d9283c26e09b6fae51fd8d4becbc999a0d595
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-06-06 14:32:24 +0200

    nettoyage createZip

--- src/FileZip.php
+++ src/FileZip.php
@@ -4,10 +4,8 @@ function createZip($repositoryPath, $repositoryName, $files)
   $filesNames = array_map('basename', $files);
 
   $zip = new ZipArchive();
-  // On ouvre l’archive.
-  if ($zip->open($repositoryPath . '/' . $repositoryName . '.zip') == TRUE)
     // On crée l’archive.
-    if ($zip->open($repositoryPath . '/' . $repositoryName . '.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
+    if ($zip->open($repositoryPath . '/' . $repositoryName . '.zip', ZipArchive::CREATE) == TRUE) {
       // Ajout d’un fichier.
       for ($i = 0; $i < count($files); $i = $i + 1) {
         if (file_exists($files[$i])) {
