fe9b348cc8a1a26e91b27a9e5138c89e2d68397e
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-06-07 22:09:27 +0200

    Envoie du second mail pour confirmation d'envoie

--- src/EnvoieMail.php
+++ src/EnvoieMail.php
@@ -19,25 +19,35 @@ $downloadLink = '<a href=http://localhost/Clone-Weetransfert/src/Download.php?fi
 
 $messageHead = 'Bonjour ' . $sendTo . ', ' . $sendFrom . ' souhaite vous transmettre des documents. Pour les télécharger, veuillez cliquer sur le lien suivant:';
 $messageFoot = 'Veuillez noter que ce lien sera valide pendant ' . $delais . 'jours. Passé ce délai, vos documents ne seront plus disponibles. Merci.';
+$messageBody = 'Bonjour ' . $sendFrom . ', le lien de téléchargement de vos fichiers à bien été envoyé à ' . $sendTo . '.<br>Merci d\'avoir utilisé CloneTransfert.';
 
 //Create an instance; passing `true` enables exceptions
-$mail = new PHPMailService;
-
-try {
+    $mail = new PHPMailService;
 
     $mail->addAddress($sendTo, '');     //Add a recipient
-
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Test Weetransfert Mailer';
     $mail->Body    = $messageHead . "<br>" . $downloadLink . "<br>" . $messageFoot;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
     //To load the French version
     $mail->setLanguage('fr', '/optional/path/to/language/directory/');
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+    if (!$mail->send()) {
+        echo 'Erreur lors de l\'envoi du premier e-mail : ' . $mail->ErrorInfo;
+    } else {
+        echo 'E-mail envoyée avec succès !';
+    }
+
+    //Envoie du second mail
+    $mail->clearAllRecipients();
+    $mail->addAddress($sendFrom, '');
+    $mail->Subject = 'Vos fichiers ont été correctement trnasférés!';
+    $mail->Body = $messageBody;
+
+    if (!$mail->send()) {
+        echo 'Erreur lors de l\'envoi du deuxième e-mail : ' . $mail->ErrorInfo;
+    } else {
+        echo 'E-mail envoyée avec succès !';
+    }
+
