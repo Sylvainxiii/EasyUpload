c712696625a396ad934ff7659590de1f05c2d6d5
Celine <celine.pro.morel@gmail.com>
2024-06-17 10:21:34 +0200

    rien

--- src/Download.php
+++ src/Download.php
@@ -1,13 +1,11 @@
 <?php
-$filepath = '../uploads/'.$_GET['file'].'/'.$_GET['file'].'.zip';
+$filepath = '../uploads/' . $_GET['file'] . '/' . $_GET['file'] . '.zip';
 
 header('Content-Description: File Transfer');
-	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename="' . basename($filepath) . '"');
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate');
-	header('Pragma: public');
-	header('Content-Length: ' . filesize($filepath));
-	readfile($filepath);
-
-?>
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="' . basename($filepath) . '"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($filepath));
+readfile($filepath);
