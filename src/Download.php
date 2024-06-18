e1661e23e3392d1264fb1c72b58590404dcbba6d
Celine <celine.pro.morel@gmail.com>
2024-06-18 09:44:11 +0200

    correction bugs liées au téléchargement

--- src/Download.php
+++ src/Download.php
@@ -2,26 +2,36 @@
 if (isset($_REQUEST["file"])) {
 	$file = urldecode($_REQUEST["file"]);
 
-	if (preg_match('/^[^.][-a-z0-9_.]+[a-z]$/i', $file)) {
+	// echo "Nom de fichier décodé : $file<br>";
+
+	if (preg_match('/^[^.][-a-z0-9_.]*$/i', $file)) {
 		$filepath = "../uploads/" . $file . '/' . $file . '.zip';
 
+		// echo "Chemin complet du fichier : $filepath<br>";
+
 		if (file_exists($filepath)) {
+			// Désactiver la sortie buffer
+			if (ob_get_level()) {
+				ob_end_clean();
+			}
+
+			// Forcer le téléchargement du fichier
 			header('Content-Description: File Transfer');
-			header('Content-Type: application/octet-stream');
+			header('Content-Type: application/zip');
 			header('Content-Disposition: attachment; filename="' . basename($filepath) . '"');
 			header('Expires: 0');
 			header('Cache-Control: must-revalidate');
 			header('Pragma: public');
 			header('Content-Length: ' . filesize($filepath));
 
-			flush();
+			// Lire et envoyer le fichier
 			readfile($filepath);
-			die();
+			exit;
 		} else {
 			http_response_code(404);
-			die();
+			die("Fichier non trouvé.");
 		}
 	} else {
-		die("Le téléchargement ne peut aboutir");
+		die("Le téléchargement ne peut aboutir : format de nom de fichier invalide.");
 	}
 }
