fc774e7eaa85a24ea267d64531cf659fd0f18620
Sylvainxiii <147145146+Sylvainxiii@users.noreply.github.com>
2024-06-18 11:00:14 +0200

    Revert "Ajout downloadPage.php"

--- src/Download.php
+++ src/Download.php
@@ -1,37 +1,13 @@
 <?php
-if (isset($_REQUEST["file"])) {
-	$file = urldecode($_REQUEST["file"]);
+$filepath = '../uploads/'.$_GET['file'].'/'.$_GET['file'].'.zip';
 
-	// echo "Nom de fichier décodé : $file<br>";
+header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Content-Disposition: attachment; filename="' . basename($filepath) . '"');
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize($filepath));
+	readfile($filepath);
 
-	if (preg_match('/^[^.][-a-z0-9_.]*$/i', $file)) {
-		$filepath = "../uploads/" . $file . '/' . $file . '.zip';
-
-		// echo "Chemin complet du fichier : $filepath<br>";
-
-		if (file_exists($filepath)) {
-			// Désactiver la sortie buffer
-			if (ob_get_level()) {
-				ob_end_clean();
-			}
-
-			// Forcer le téléchargement du fichier
-			header('Content-Description: File Transfer');
-			header('Content-Type: application/zip');
-			header('Content-Disposition: attachment; filename="' . basename($filepath) . '"');
-			header('Expires: 0');
-			header('Cache-Control: must-revalidate');
-			header('Pragma: public');
-			header('Content-Length: ' . filesize($filepath));
-
-			// Lire et envoyer le fichier
-			readfile($filepath);
-			exit;
-		} else {
-			http_response_code(404);
-			die("Fichier non trouvé.");
-		}
-	} else {
-		die("Le téléchargement ne peut aboutir : format de nom de fichier invalide.");
-	}
-}
+?>
