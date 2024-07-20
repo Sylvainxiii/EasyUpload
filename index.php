6e880ad30fcafdee9a8f622dd692a057b165b973
John <johntchen.mos@gmail.com>
2024-07-20 02:44:33 +0200

    Add Log
     Ajout Log TimeStamp
     Ajout Paramètres Log

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require 'vendor/autoload.php';
 
 include_once 'src/dotEnv.php';
+include_once 'src/log.php';
+
 dotEnv(__DIR__);
 
 
@@ -19,14 +21,13 @@ include 'src/_header.php';
 				<div class="custom-file">
 					<label class="custom-file-label" for="fichier" id="fileNameLabel">Choisir des fichiers</label>
 					<input type="file" class="custom-file-input" id="fichier" name="files[]" multiple required>
-
 				</div>
 			</div>
 			<div class="mb-3">
 				<label for="destEmail" class="form-label">Email destinataire: <span class="email-count">0</span></label>
 				<div class="email-list"></div>
 
-				<input type="email" placeholder="example@example.com" value='' class="form-control custom-input" id="destEmail" name="destEmail">
+				<input type="email" value='' placeholder="example@example.com"  class="form-control custom-input" id="destEmail" name="destEmail">
 			</div>
 			<div class="mb-3">
 				<label for="sourceEmail" class="form-label">Votre Email</label>
