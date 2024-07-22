c48fe2c777cd41768a23f4a99d0e2d44cab0edde
John <johntchen.mos@gmail.com>
2024-07-22 20:17:52 +0200

    Modifs mineurs bouton send Envoyer index php champ url js renommer var JS email variables     eMailDom emailDestinataireDom     sourceEmailDom =>emailExpediteurDom Supression du dossier logs Ajout du fichier log dans les fichiers Download , EnvoiEmail, FileZip, bddCrud(Model), downloadPage, upload Extraction HTML de login vers pageLogin Log Trace

--- src/accueil.php
+++ src/accueil.php
@@ -1,27 +1,25 @@
-	<div class="form">
+<div class="form">
 		<form id="uploadForm" method="post" enctype="multipart/form-data">
 			<input type="hidden" name="action" value="create">
 			<div class="mb-3">
 				<div class="custom-file">
 					<label class="custom-file-label" for="fichier" id="fileNameLabel">Choisir des fichiers</label>
-					<input type="file" class="custom-file-input" id="fichier" name="files[]" multiple required onchange='updateFileName()'>
+					<input type="file" class="custom-file-input" id="fichier" name="files[]" multiple required>
+
 				</div>
 			</div>
 			<div class="mb-3">
 				<label for="destEmail" class="form-label">Email destinataire: <span class="email-count">0</span></label>
 				<div class="email-list"></div>
 
-				<button class="email-add">🞦</button>
-				<!-- use attribute `multiple` ? -->
-				<input type="email" placeholder="example@example.com" value='' class="form-control custom-input" id="destEmail" name="destEmail">
+				<input type="email" value='' placeholder="example@example.com"  class="form-control custom-input" id="destEmail" name="destEmail">
 			</div>
 			<div class="mb-3">
-				<label for="sourceEmail" class="form-label">Votre Email</label>
-				<input type="email" value='' class="form-control custom-input" id="sourceEmail" name="sourceEmail" required>
+				<label for="expediteurEmail" class="form-label">Votre Email</label>
+				<input type="email" value='' class="form-control custom-input" id="expediteurEmail" name="expediteurEmail" required>
 			</div>
 			<div>
-				<button type="submit" class="btn btn-primary" id="send" name="submit" value="send" disabled>Send</button>
+				<input type="submit" class="btn btn-primary" id="send" name="submit" value="Envoyer" disabled>
 			</div>
-
 		</form>
 	</div>
--- src/log.php
+++ src/log.php
@@ -5,46 +5,37 @@ function getTimeStampLog(){
     return date("Y-m-d H:i:s");
 }
 
-function setLevelLog(int $result){
-    try{
-        switch($result){
-            case 1:
-                $result = 'TRACE';
-                break;
-            case 2:
-                $result = 'DEBUG';
-                break;
-            case 3:
-                $result = 'INFO';
-                break;
-            case 4:
-                $result = 'WARNING';
-                break;  
-            case 5:
-                $result = 'ERROR';
-                break;
-        }
-    }
-    catch(Exception $e){
-        echo "Erreur de typage du niveau";
-    }
-    return $result;
-}
+// function setLevelLog(int $result){
+
+//     try{
+//         switch($result){
+//             case TRACE:
+//                 $result = 'TRACE';
+//                 break;
+//             case INFO:
+//                 $result = 'INFO';
+//                 break;
+//         }
+//     }
+//     catch(Exception $e){
+//         echo "Erreur de typage du niveau";
+//     }
+//     return $result;
+// }
 
 
 function getCurrentFileLog(){
     return $_SERVER['SCRIPT_NAME'];
 }
 
-function setLog(int $level, string $message){
+function setLog(string $message, string $level="TRACE"){
     try{
         $directory = '../var/logs/';
         $filePath = "$directory/log.log";
         $currentFile = getCurrentFileLog();
-        $level = setLevelLog($level);
         $user = get_current_user();
         $date = getTimeStampLog();
-        $text = sprintf("[%s] %s %s %s %s \r\n",$date, $level,$user, $currentFile, $message);
+        $text = sprintf("[%s] %s %s %s %s \r\n",$date, $level, $user, $currentFile, $message);
     
         // creer dossier
         if(!is_dir($directory)){
